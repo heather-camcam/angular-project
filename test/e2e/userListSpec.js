3e5d1309ac96426793cdfd0ea6b2dcd694a141db
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-10 16:19:04 +0000

    user story 3: feature test for more than one user

--- test/e2e/userListSpec.js
+++ test/e2e/userListSpec.js
@@ -11,5 +11,7 @@ describe ("User list", function(){
     browser.get("http://localhost:8000");
     var userAvatarUrl = element.all(by.css('.user-avatar')).first().getAttribute('src');
     expect(userAvatarUrl).toEqual("https://avatars.githubusercontent.com/u/1?v=3");
+    var userAvatarUrlThree = element.all(by.css('.user-avatar')).last().getAttribute('src');
+    expect(userAvatarUrlThree).toEqual("https://avatars.githubusercontent.com/u/3?v=3");
   });
 });

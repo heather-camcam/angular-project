592814111885c7960ef8afcd12bad27ad93e772e
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-10 16:15:30 +0000

    user story 3

--- test/e2e/userListSpec.js
+++ test/e2e/userListSpec.js
@@ -6,4 +6,10 @@ describe ("User list", function(){
     expect(users.count()).toBe(3);
     expect(users.first().getText()).toEqual('mojombo');
   });
+
+  it ("displays an avatar for each user", function(){
+    browser.get("http://localhost:8000");
+    var userAvatarUrl = element.all(by.css('.user-avatar')).first().getAttribute('src');
+    expect(userAvatarUrl).toEqual("https://avatars.githubusercontent.com/u/1?v=3");
+  });
 });

844132793f389afef39077a7e8396a134f888753
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-10 15:18:46 +0000

    user story 1: list of github users

--- test/e2e/userListSpec.js
+++ test/e2e/userListSpec.js
@@ -1,8 +1,9 @@
-// describe ("User list", function(){
-//
-//   // it ("displays a list of users", function(){
-//   //   browser.get("http://localhost:8000");
-//   //   var users = element.all(by.css('.userName'));
-//   //   expect(users.length).toBe(3);
-//   // });
-// });
+describe ("User list", function(){
+
+  it ("displays a list of users", function(){
+    browser.get("http://localhost:8000");
+    var users = element.all(by.css('.user-profile'));
+    expect(users.count()).toBe(3);
+    expect(users.first().getText()).toEqual('mojombo');
+  });
+});

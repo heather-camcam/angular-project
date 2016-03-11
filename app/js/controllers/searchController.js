db1a66b357a895c078bb56e1e5e6c73604e733b5
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-11 15:00:12 +0000

    Refactored feature tests into showing avatars; showing user followers; and search as seperate features

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -9,28 +9,8 @@ githubUserSearch.controller("searchController", ['$resource', function($resource
 
 
 
-  self.searchResult = []
-    // {
-    //   "login": "mojombo",
-    //   "id": 1,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/1?v=3",
-    //   "public_repos": 10,
-    //   "followers": 10
-    // },
-    // {
-    //   "login": "defunkt",
-    //   "id": 2,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/2?v=3",
-    //   "public_repos": 20,
-    //   "followers": 20
-    // },
-    // {
-    //   "login": "pjhyett",
-    //   "id": 3,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/3?v=3",
-    //   "public_repos": 30,
-    //   "followers": 30
-    // }
+  self.searchResult = [];
+
 
   self.makeSearch = function(){
 
@@ -38,10 +18,9 @@ githubUserSearch.controller("searchController", ['$resource', function($resource
       {q: self.searchTerm}).$promise.then(function(data){
         self.searchResult =  data.items;
         console.log(data.items);
-      })
+      });
     };
-  //   console.log(self.searchResult)
-  // }
+
 
   self.doSearch = function() {
     console.log('hello world');

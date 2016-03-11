19bee482f9cccb4f7457172082ce82e9fef1976b
Michael Collins <michaelj.collinsuk@gmail.com>
2016-03-11 16:15:27 +0000

    Add second api request

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -5,49 +5,24 @@ githubUserSearch.controller("searchController", ['$resource', function($resource
 
   var searchResource = $resource('https://api.github.com/search/users');
 
-
-
-
-
   self.searchResult = []
-    // {
-    //   "login": "mojombo",
-    //   "id": 1,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/1?v=3",
-    //   "public_repos": 10,
-    //   "followers": 10
-    // },
-    // {
-    //   "login": "defunkt",
-    //   "id": 2,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/2?v=3",
-    //   "public_repos": 20,
-    //   "followers": 20
-    // },
-    // {
-    //   "login": "pjhyett",
-    //   "id": 3,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/3?v=3",
-    //   "public_repos": 30,
-    //   "followers": 30
-    // }
+  self.userResult = []
+  self.followers = []
+  self.repos = []
 
-  self.makeSearch = function(){
+    self.makeSearch = function(){
 
-    searchResource.get(
-      {q: self.searchTerm}).$promise.then(function(data){
-        self.searchResult =  data.items;
-        console.log(data.items);
+      searchResource.get(
+        {q: self.searchTerm, access_token: }).$promise.then(function(data){
+          self.searchResult =  data.items;
+      })
+
+      var other = $resource('https://api.github.com/users/' + 'mojombo' + '?access_token=');
+
+      other.get().$promise.then(function(data){
+          self.followers =  data.followers;
+          self.repos = data.public_repos;
       })
     };
-  //   console.log(self.searchResult)
-  // }
-
-  self.doSearch = function() {
-    console.log('hello world');
-  };
-
-
-
 
 }]);
